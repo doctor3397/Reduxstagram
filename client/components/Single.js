8c2eb6c8963e1423663be8559936120c9f4daa76
Theresa Shen <tshen@architech.ca>
2018-05-17 16:59:00 -0400

    Display the comments and add a comment form to add comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,14 +4,17 @@ import Comments from "./Comments";
 
 class Single extends React.Component {
   render() {
+    const { postId } = this.props.params;
     // Get the index of the post
-    const i = this.props.posts.findIndex((post) =>  post.code === this.props.params.postId);
+    const i = this.props.posts.findIndex((post) =>  post.code === postId);
     // Get the post with the index
     const post = this.props.posts[i];
+    // Get the comments for this post
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props}/>
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
