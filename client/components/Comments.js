4025b8af35810efd801506e2c3861fa72be3f1bf
Theresa Shen <tshen@architech.ca>
2018-05-17 23:04:01 -0400

    Create the onSubmit event listener and call addComment function to create new comment

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class Comments extends React.Component {
+  constructor(){
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   renderComment(comment, i) {
     // console.log(comment);
     return(
@@ -13,11 +17,20 @@ class Comments extends React.Component {
       </div>
     )
   };
+  handleSubmit(e) {
+     e.preventDefault();
+     // console.log(this.refs);
+     const { postId } = this.props.params;
+     const author = this.refs.author.value;
+     const comment = this.refs.comment.value;
+     // console.log(postId, author, comment);
+     this.props.addComment(postId, author, comment);
+   };
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
           <input type="submit" hidden/>
